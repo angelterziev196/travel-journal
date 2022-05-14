ccdd0a7b72d58527f4236f4bc938d468b1f3f313
AngelTerziev <angelterziev195@gmail.com>
2022-05-14 15:59:43 +0300

    main

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import './styles/index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/destinations/Destination';
+import Destinations from './components/destinations/Destination';
+import data from 'D:/Coding/travel-journal/src/data';
+import Nav from 'D:/Coding/travel-journal/src/components/nav/Nav';
+
+export default function App() {
+    const destination = data.map((dest) => {
+        return <Destinations key={dest.key} {...dest} />;
+    });
+    return (
+        <div className='wrapper'>
+            <Nav />
+            {destination}
+        </div>
+    );
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
